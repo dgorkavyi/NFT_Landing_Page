a0ca861913e550002e4eb45943556c29b8b3b34d
Dmytro <dima.net.dd@gmail.com>
2023-02-19 16:28:53 +0200

    remove all and move to root

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import * as webpFuncs from './modules/webpFunctions.js';
-
-webpFuncs.isWebp();
-
-$(document).ready(function () {
-  $('.feedback__slider').slick({});
-  $('.slick-prev').html('<img src="img/arrow-left.svg" />');
-  $('.slick-next').html('<img src="img/arrow-right.svg" />');
-});
